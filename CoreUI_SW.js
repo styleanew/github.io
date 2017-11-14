b37de668257ffa05368dead24b9179d2f60b54d5
zoltan <zkubanyi@gmail.com>
2017-11-14 10:40:35 -0800

    Added service worker for Firebase Functin. Added launching demo sites in iFrame. Made some minor style changes to reduce the modal margin.

---
+++ CoreUI_SW.js
@@ -0,0 +1,12 @@
+
+
+importScripts('https://www.gstatic.com/firebasejs/3.6.6/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.6.6/firebase-messaging.js');
+
+// Initialize the Firebase app in the service worker by passing in the
+// messagingSenderId.
+firebase.initializeApp({
+  // Add messagingSenderId
+  messagingSenderId: '470145900753';
+});
+var messaging = firebase.messaging();
